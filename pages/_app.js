b1960e059c9a71277394ff9972b6e8cf12336c5b
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-06-14 13:15:22 +0200

    updating styles and layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+//wrapper component for whole app. place to import global styles
 import '../styles/globals.css'
 export default function App( {Component, pageProps} ) {
     return <Component {...pageProps} />
