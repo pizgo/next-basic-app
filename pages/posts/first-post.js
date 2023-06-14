20ad76a07649a2efa81f49e19d1446c716ba4e09
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-06-14 12:25:31 +0200

    adding title, new page, Link

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,17 @@
+import Link from 'next/link';
+import Image from 'next/image'
+import Head from "next/head";
+
 export default function FirstPost() {
-    return <h1>First Post</h1>
+    return (
+        <>
+            <Head>
+                <title>First post</title>
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+        </>
+    );
 }
